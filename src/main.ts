1a85dab2e889c4f528f3b0258800e646f34cc474
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-07 08:09:43 -0300

    el-icons

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import './assets/main.css'
+import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import ptBr from 'element-plus/dist/locale/pt-br.mjs'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(ElementPlus, { locale: ptBr })
 app.use(router)
+app.use(ElementPlus, { locale: ptBr })
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
