b3d8105c06bb54f4d4fdfd26b903d85e7b3f6cd5
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-07 11:00:32 -0300

    test actions

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './assets/main.css'
 import 'element-plus/dist/index.css'
-import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import ptBr from 'element-plus/dist/locale/pt-br.mjs'
+
 import App from './App.vue'
 import router from './router'
+import { createApp } from 'vue'
 
 const app = createApp(App)
 
