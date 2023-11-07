127862719d7345878ca3988fab4ca62ad1d7e3bd
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-07 10:12:45 -0300

    dark theme

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import './assets/main.css'
-import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'element-plus/dist/index.css'
 import ptBr from 'element-plus/dist/locale/pt-br.mjs'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
 app.use(ElementPlus, { locale: ptBr })
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
+app.use(router)
 
 app.mount('#app')
