b794b77fa9a53ff61d6b83a7706d7d173be960ea
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-08 16:23:39 -0300

    seashells theme created

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,25 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+        {
+          seashells: {
+            "primary": "#1E4962",
+            "secondary": "#DEB88D",
+            "accent": "#027C9B",
+            "neutral": "#17384C",
+            "base-100": "#09141B",
+            "base-content": "#FEE4CE",
+            "info": "#68D4F1",
+            "success": "#36d399",
+            "warning": "#FCA02F",
+            "error": "#D15123",
+          },
+        },
+        "lemonade",
+      ],
+  }
 }
 
