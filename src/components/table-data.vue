0aaff4c45a8fe6201d8842ed4228c847f56764ae
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-08 14:57:15 -0300

    initial set up

---
+++ src/components/table-data.vue
@@ -0,0 +1,40 @@
+<script async setup lang="ts">
+import { computed, ref, onBeforeMount } from 'vue'
+import { api } from '../utils/api'
+
+const tableData = ref()
+const loading = ref(true)
+onBeforeMount(async () => {
+  const response = await api.login()
+  const dados = await api.get('relatorios/', response?.token)
+  tableData.value = dados.results
+  loading.value = false
+})
+
+const search = ref('')
+const filterTableData = computed(() => {
+  if (!tableData.value) return []
+
+  return tableData.value.filter(
+    (data: { local: string }) =>
+      !search.value ||
+      data.local.toLowerCase().includes(search.value.toLowerCase())
+  )
+})
+
+const dataFormatter = (row: { data: string }) => {
+  const dataFormatada = new Intl.DateTimeFormat('pt-BR', {
+    dateStyle: 'long',
+    timeStyle: 'short',
+    timeZone: 'America/Bahia'
+  }).format(new Date(row.data))
+
+  return dataFormatada
+}
+</script>
+
+<template>
+  <Transition name="slide-fade" mode="out-in">
+    table
+  </Transition>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-import 'element-plus/theme-chalk/dark/css-vars.css'
 import './assets/main.css'
-import 'element-plus/dist/index.css'
-import ElementPlus from 'element-plus'
-import ptBr from 'element-plus/dist/locale/pt-br.mjs'
 
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createApp } from 'vue'
 
 const app = createApp(App)
 
-app.use(ElementPlus, { locale: ptBr })
 app.use(router)
 
 app.mount('#app')
