776be8053ff3f1e416e158201fcccdd5d347841a
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-11 14:23:28 -0300

    topbar redesigned to full squared

--- src/components/top-bar.vue
+++ src/components/top-bar.vue
@@ -3,7 +3,7 @@
     <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content flex flex-col">
       <!-- Navbar -->
-      <div class="navbar bg-neutral shadow-lg rounded-3xl p-3">
+      <div class="navbar bg-neutral shadow-lg">
         <div class="flex-none sm:hidden">
           <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
             <span class="material-symbols-outlined">
@@ -19,7 +19,7 @@
         <div class="flex-none">
           <ul class="menu menu-horizontal px-1 gap-3">
             <li>
-              <label class="swap swap-rotate w-9 h-9">
+              <label class="swap swap-rotate w-9 h-9" title="Trocar Tema" aria-label="Trocar Tema">
                 <input type="checkbox" data-toggle-theme="seashells,sunrise" data-act-class="ACTIVECLASS" />
                 <span class="material-symbols-outlined swap-on fill-current">
                   light_mode
@@ -29,9 +29,9 @@
                 </span>
               </label>
             </li>
-            <li class="hidden sm:block"><router-link to="/">Início</router-link></li>
-            <li class="hidden sm:block"><router-link to="/relatorios/">Relatórios</router-link></li>
-            <li class="hidden sm:block"><router-link to="/settings/">Configurações</router-link></li>
+            <li class="hidden sm:block"><router-link class="nav-link" to="/">Início</router-link></li>
+            <li class="hidden sm:block"><router-link class="nav-link" to="/relatorios/">Relatórios</router-link></li>
+            <li class="hidden sm:block"><router-link class="nav-link" to="/settings/">Configurações</router-link></li>
             <li class="hidden sm:block"><button>Log out</button></li>
           </ul>
         </div>
