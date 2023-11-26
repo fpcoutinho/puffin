59d67ea5029c63d506d808864b454fb58da92e85
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-26 08:19:40 -0300

    image carousel

---
+++ src/components/image-carousel.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="carousel w-full">
+    <div v-for="imagem, index in  props.imagens" :key="imagem.id" :id="'slide' + index"
+      class="carousel-item relative w-full">
+      <img :src="imagem.img" class="w-full" />
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a v-if="(index - 1) > 0" :href="'#slide' + (index - 1)" class="btn btn-circle">❮</a>
+        <a v-else :href="'#slide' + (props.imagens.length - 1)" class="btn btn-circle">❮</a>
+        <a v-if="(index + 1) < props.imagens.length" :href="'#slide' + (index + 1)" class="btn btn-circle">❯</a>
+        <a v-else href="#slide0" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+  </div>
+  <div class="flex justify-center w-full py-2 gap-2">
+    <a v-for="imagem, index in  imagens" :key="imagem.id" :href="'#slide' + index" class="btn btn-xs">{{ index + 1 }}</a>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  imagens: {
+    type: Array as () => { id: string; img: string }[],
+    required: true
+  }
+})
+
+</script>
+
+<style>
+@import '@/assets/component-styles/carousel.scss';
+</style>
--- src/views/relatorio-detailed-view.vue
+++ src/views/relatorio-detailed-view.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="prose max-w-none flex flex-col gap-4 items-center">
     <h1>Relatorio {{ props.id }}</h1>
+    <p><b>Data:</b> {{ data() }}</p>
+    <p><b>Hora:</b> {{ hora() }}</p>
+    <p><b>Local:</b> {{ relatorio.local }}</p>
+    <p><b>Temperatura:</b> {{ relatorio.temperatura }} °C</p>
+    <p><b>Condições Climáticas:</b> {{ relatorio.clima }} </p>
+    <p><b>Responsáveis pela Inspeção:</b> {{ relatorio.responsaveis }}</p>
+
+    <div v-if="showImagens()">
+      <h3>Imagens</h3>
+      <image-carousel :imagens="imagens" />
+    </div>
+    <div v-if="showCircuitos()">
+      <h2>Circuitos</h2>
+      <div class="grid grid-cols-2 gap-4">
+        <div v-for="circuito in circuitos" :key="circuito">
+          <p>{{ circuito }}</p>
+        </div>
+      </div>
+    </div>
     <p v-for="value, key in relatorio" :key="key">
       {{ key }}: {{ value }}
     </p>
@@ -10,18 +29,41 @@
 <script setup lang="ts">
 import { onBeforeMount, ref } from 'vue';
 import { api } from '../utils/api';
+import { format } from '../utils/format';
+import ImageCarousel from '@/components/image-carousel.vue';
 
 const props = defineProps({
   id: String
 })
 
-const relatorio = ref()
+const relatorio = ref<any>({})
+const imagens = ref<{ id: string; img: string }[]>([])
+const circuitos = ref([])
 
 onBeforeMount(async () => {
-  relatorio.value = await api.get(`relatorios/${props.id}`)
-  relatorio.value = relatorio.value.data
+  const relatorioResponse = await api.get(`relatorios/${props.id}`)
+  relatorio.value = relatorioResponse?.data
+
+  const imagensResponse = await api.get(`relatorios/${props.id}/imagens`)
+  imagens.value = imagensResponse?.data.results
+
+  const circuitosResponse = await api.get(`relatorios/${props.id}/circuitos`)
+  circuitos.value = circuitosResponse?.data.results
 })
 
-</script>
+const data = () => {
+  return format.data(relatorio.value.data).split('às')[0]
+}
 
-<style scoped></style>
+const hora = () => {
+  return format.data(relatorio.value.data).split('às')[1]
+}
+
+const showImagens = () => {
+  return imagens.value.length > 0
+}
+
+const showCircuitos = () => {
+  return circuitos.value.length > 0
+}
+</script>
